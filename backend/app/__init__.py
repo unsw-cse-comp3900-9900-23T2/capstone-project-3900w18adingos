8d02978fe1895f120dcc2d40eba78bd886937c95
Jason Gong <jasongong1@outlook.com>
2023-06-22 22:53:46 +1000

    remove unused imports

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
-from app.models.customer import Customer
-from app.models.manager import Manager
-
 from .database import db
-from .auth import auth_blueprint
 from .mail import init_mail
-
-
 
 def create_app():
 
