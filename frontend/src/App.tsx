6f33c6a5f97bc44316bde3a986a8e5b413477bac
Sam Evstigneev <119456083+Donnie-1@users.noreply.github.com>
2023-06-22 15:57:46 +1000

    updated wrappers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import "./components/Map.css"
 import Map from "./components/Map"
+
 const App = () => { 
     return ( 
         <>
         <div className="App">
           <header className="App-header">
-            <h1>MAAAAAp🇦🇫</h1>
           </header>
-          <div className="map-wrapper">
+          <div className="wrapper">
             <Map />
           </div>
         </div>
