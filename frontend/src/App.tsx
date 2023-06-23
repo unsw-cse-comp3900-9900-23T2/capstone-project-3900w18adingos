04f6d888cca0c33b9b092d3cd21795a10a09cdce
Sam Evstigneev <119456083+Donnie-1@users.noreply.github.com>
2023-06-23 12:38:31 +1000

    init routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import "./styles/Map.css"
-import Map from "./components/Map"
+import SignUp from "./pages/SignUp";
 import MapHomePage from "./pages/MapHomePage"
+
+import { Routes, Route, BrowserRouter} from "react-router-dom";
+
 const App = () => { 
     return ( 
-        <>
-        <MapHomePage />
-      </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<MapHomePage />} />
+      </Routes>
+    </BrowserRouter>
     );
 }
 
