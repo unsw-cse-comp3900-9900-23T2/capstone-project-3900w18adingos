8f2ea3971b7a6e67f8999d0bf6bca694fe5ea88b
Jason Gong <jasongong1@outlook.com>
2023-06-25 00:10:00 +1000

    change location attribute to be separate lat long float

--- backend/app/models/eatery.py
+++ backend/app/models/eatery.py
@@ -10,7 +10,8 @@ class Eatery(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128))
     restaurant_name = db.Column(db.String(100))
-    location = db.Column(db.Text)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
     reviews = db.relationship('Review', backref='eatery')
     #restaurant_pics = db.Column(db.String(500))  # this can be a string of URLs
 
