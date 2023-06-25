a6124444e337a2b0116a032cf364388f340269f3
Jason Gong <jasongong1@outlook.com>
2023-06-26 01:22:53 +1000

    add new image db class

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,6 +9,7 @@ from app.models.has_voucher import HasVoucher
 from app.models.voucher import Voucher
 from app.models.eatery import Eatery
 from app.models.customer import Customer
+from app.models.image import Image
 
 def create_app(config_name='default'):
 
@@ -31,6 +32,8 @@ def create_app(config_name='default'):
         from app.user import user as user_blueprint
         app.register_blueprint(user_blueprint)
     
+        from app.eatery import eatery as eatery_blueprint
+        app.register_blueprint(eatery_blueprint)
 
         from app.review import review as review_blueprint
         app.register_blueprint(review_blueprint)
