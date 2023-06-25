0f5658caccf248a610ce7bc853ea9c972763f112
Yskak <shiyap.yskak@gmail.com>
2023-06-25 11:12:31 +1000

    fixed logout and resetpassword

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -3,15 +3,25 @@ from app import auth_helper
 
 auth = Blueprint('auth', __name__)
 
+
+def check_role(role):
+    if role not in ['customer', 'eatery']:
+        return False
+    return True
+
+
 @auth.route('/auth/login', methods=['POST'])
 def login():
     email = request.json.get('email')
     password = request.json.get('password')
     role = request.json.get('role')
-    if role not in ['customer', 'eatery']:
+
+    if not check_role(role):
         return jsonify({"message": "Invalid role"}), 400
+
     result = auth_helper.auth_login(email, password, role)
     return result
+
 
 @auth.route('/auth/register', methods=['POST'])
 def register():
@@ -24,19 +34,26 @@ def register():
     result = auth_helper.auth_register(email, password, name, role)
     return result
 
+
 @auth.route('/auth/logout', methods=['POST'])
 def logout():
     token = request.json.get('token')
     result = auth_helper.auth_logout(token)
     return result
 
+
 @auth.route('/auth/passwordreset/request', methods=['POST'])
 def passwordreset_request():
     email = request.json.get('email')
-    result = auth_helper.auth_passwordreset_request(email)
+    role = request.json.get('role')
+    if not check_role(role):
+        return jsonify({"message": "Invalid role"}), 400
+
+    result = auth_helper.auth_passwordreset_request(email, role)
     return result
 
-@auth.route('/auth/passwordreset/reset', methods=['POST'])
+
+@auth.route('/auth/passwordreset/reset/', methods=['POST'])
 def passwordreset_reset():
     reset_code = request.json.get('reset_code')
     new_password = request.json.get('new_password')
