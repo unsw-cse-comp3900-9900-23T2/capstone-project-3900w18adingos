2b9ad01f115eb2207c7f2b59cb7f378752f9c464
Jason Gong <jasongong1@outlook.com>
2023-06-28 16:06:16 +1000

    correct definition for filepath column

--- backend/app/models/image.py
+++ backend/app/models/image.py
@@ -3,6 +3,6 @@ from app.extensions import db
 class Image(db.Model):
     __tablename__ = 'image'
     id = db.Column(db.Integer, primary_key=True)
-    filepath = db.Text()
+    filepath = db.Column(db.Text())
     eatery_id = db.Column(db.Integer(), db.ForeignKey('eatery.id'))
 
