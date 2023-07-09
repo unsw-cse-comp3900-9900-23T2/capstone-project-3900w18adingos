fb4e2f35b88143761b7fcca96a50d97558c94432
Yskak <shiyap.yskak@gmail.com>
2023-07-09 19:51:30 +1000

    get rid of the comment

--- backend/app/user.py
+++ backend/app/user.py
@@ -6,68 +6,7 @@ from app.extensions import db
 from app.auth_helper import user_is_eatery
 
 
-
 user = Blueprint('user', __name__)
-
-# another way to do it with user_is_eatery function, so just have one get_profile
-
-# @user.route('/profile/', methods=['GET'])
-# def get_profile():
-#     if user_is_eatery(current_user):
-#         eatery = current_user._get_current_object()
-#         eatery_data = {
-#             'id': eatery.id,
-#             'restaurant_name': eatery.restaurant_name,
-#             'location': eatery.location,
-#             'email': eatery.email,
-#             'cuisine': eatery.cuisine
-#         }
-#         return jsonify(eatery_data)
-#     else:
-#         customer = current_user._get_current_object()
-#         customer_data = {
-#             'id': customer.id,
-#             'name': customer.name,
-#             'email': customer.email,
-#             'password': customer.password,
-#             'password_hash': customer.password_hash,
-#             'handle': customer.handle,
-#             'profile_pic': customer.profile_pic
-#         }
-#         return jsonify(customer_data)
-
-# @user.route('/edit-profile/', methods=['PUT'])
-# def edit_profile():
-#     token = request.json.get('token')
-#     user = None
-    
-#     if user_is_eatery():
-#         user = Eatery.verify_auth_token(token)
-#     else:
-#         user = Customer.verify_auth_token(token)
-
-#     if not user:
-#         return jsonify({"message": "Token is invalid"}), 400
-
-#     data = request.get_json()
-    
-#     if user_is_eatery():
-#         user.restaurant_name = data.get('restaurant_name', user.restaurant_name)
-#         user.location = data.get('location', user.location)
-#     else:
-#         user.name = data.get('name', user.name)
-#         user.handle = data.get('handle', user.handle)
-#         user.profile_pic = data.get('profile_pic', user.profile_pic)
-    
-#     user.email = data.get('email', user.email)
-
-#     if 'password' in data:
-#         user.password = data.get('password', user.password)
-#         user.hash_password(data.get('password'))
-
-#     db.session.commit()
-#     return jsonify({"message": "Profile updated"}), 200
-
 
 @user.route('/customer/profile/', methods=['GET'])
 def get_customer():
