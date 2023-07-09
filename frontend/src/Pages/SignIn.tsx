1549239876b21f419b603dd35f9c18c9292c7d50
Yskak <shiyap.yskak@gmail.com>
2023-07-09 19:24:55 +1000

    added forgot password pages

--- frontend/src/Pages/SignIn.tsx
+++ frontend/src/Pages/SignIn.tsx
@@ -8,38 +8,15 @@ import { SignInFormInputs } from '../interface';
 
 const SignIn: React.FC = () => {
   const { register, handleSubmit } = useForm<SignInFormInputs>();
-  
-  const [toggleResetPasswordOptions, setToggleResetPasswordOptions] = useState(false);
-  const { user, passwordResetRequest, passwordReset } = useAuth();
-  const [resetCode, setResetCode] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-
-  const [message, setMessage] = useState("");
   const [role, setRole] = useState<string>("");
-
+  const [message, setMessage] = useState("");
   const { login } = useAuth();
   const navigate = useNavigate();
 
-  const handlePasswordResetRequest = async (data: SignInFormInputs) => {
-    const {email} = data;
-    if (!email) { 
-      setMessage("please enter email in first input box"); 
-    } else if (!role) { 
-      setMessage("please select a role - Customer or Resturant Owner?"); 
-    } else { 
-      console.log(email + " " + role)
-      const result = await passwordResetRequest(email, role);
-    }
-  };
-  
-  const handlePasswordReset = async () => {
-    const result = await passwordReset(resetCode, newPassword);
-    if (result) {
-    }
-  };
   
   const onSubmit = async (data: SignInFormInputs) => {
     const { email, password } = data;
+
     try {
       const success = await login(email, password, role);
       if (success) { 
@@ -63,7 +40,7 @@ const SignIn: React.FC = () => {
       <form onSubmit={handleSubmit(onSubmit)} className="signup-form">
         <input {...register("email")} placeholder="Email" type="email" className="input-field" />
         <input {...register("password")} placeholder="Password" type="password" className="input-field" />
-        
+
         <div className="user-type-select">
           <button 
               type="button" 
@@ -78,24 +55,7 @@ const SignIn: React.FC = () => {
               I'm a Resturant Owner
           </button>
         </div>
-
-        {!toggleResetPasswordOptions && 
-          <button type="submit" className="submit-button">Sign In</button>
-        }
-        
-        {toggleResetPasswordOptions &&
-          <div className="toggle-reset-password-container">
-              <input type="text" onChange={(e) => setResetCode(e.target.value)} className='input-field' placeholder="Enter Reset Code" />
-              <button onClick={handleSubmit(handlePasswordResetRequest)} className='reset-button'>Send Code</button>
-
-              <input type="password" onChange={(e) => setNewPassword(e.target.value)} className='input-field' placeholder="New Password" />
-              <button onClick={handlePasswordReset}className='reset-button' >Reset Password</button>
-            </div>
-          }
-
-        <div className='forgot-password' onClick={() => {setToggleResetPasswordOptions(!toggleResetPasswordOptions)}}>
-          <p className='forgot-password-text'>forgot password</p>
-        </div>
+        <button type="submit" className="submit-button">Sign In</button>
       </form>
       {message && <p>{message}</p>}
     </div>
