6920b15d45fd010c11f4fa0411cffb3a0df3598b
Jason Gong <jasongong1@outlook.com>
2023-07-14 17:16:32 +1000

    initialize marshmallow

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+ma = Marshmallow()
+
 login_manager = LoginManager()
