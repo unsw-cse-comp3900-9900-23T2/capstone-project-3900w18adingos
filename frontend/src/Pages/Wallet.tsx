3418f15c24703e527e7001abcddb2d29218c81cc
Yskak <shiyap.yskak@gmail.com>
2023-07-16 16:49:13 +1000

    added collapse

--- frontend/src/Pages/Wallet.tsx
+++ frontend/src/Pages/Wallet.tsx
@@ -1,13 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Card, Button, Collapse } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import "../styles/Wallet.css";
 import qrCode from "../assets/qr-code.png";
 import Footer from '../components/Footer/Footer';
 import Header from "../components/Header/Header";
-import { useNavigate } from 'react-router-dom';  // Import useHistory
-import { useEffect} from 'react';
 
 const Wallet: React.FC = () => {
   const navigate = useNavigate();
-  const checkToken = localStorage.getItem('token')
+  const checkToken = localStorage.getItem('token');
 
   useEffect(() => {
     if (!checkToken) {
@@ -19,6 +20,9 @@ const Wallet: React.FC = () => {
     return null;
   }
 
+  const [openStarBucks, setOpenStarBucks] = useState(false);
+  const [openStellinis, setOpenStellinis] = useState(false);
+
   return (
     <>
       <Header>
@@ -29,104 +33,64 @@ const Wallet: React.FC = () => {
           <img src={qrCode} alt="qr code" />
         </div>
         <div className="wallet-accordian">
-          <div className="panel-group" id="accordion">
-            <div className="panel panel-default">
-              <div
-                className="panel-heading"
-                data-toggle="collapse"
-                data-parent="#accordion"
-                // href="#collapse2"
-              >
-                <h4 className="panel-title">
-                  <a
-                    className="accordion-toggle"
-                    data-toggle="collapse"
-                    data-parent="#accordion"
-                    href="#collapse2"
-                  >
-                    Star Bucks
-                    <div className="ratings">
-                      <i className="bi bi-star-fill"></i>
-                      <i className="bi bi-star-fill"></i>
-                      <i className="bi bi-star-fill"></i>
-                      <i className="bi bi-star-half"></i>
-                      <i className="bi bi-star-half"></i>
-                    </div>
-                  </a>
-                </h4>
-                <div className="panel-bottom">
-                  <h5>10% off Coffee</h5>
-                  <span>25%</span>
-                </div>
-                <i className="bi bi-chevron-down arrow-down"></i>
-              </div>
-              <div id="collapse2" className="panel-collapse collapse">
-                <div className="panel-body">
-                  <h3>Star Bucks</h3>
-                  <div className="ratings">
-                    <i className="bi bi-star-fill"></i>
-                    <i className="bi bi-star-fill"></i>
-                    <i className="bi bi-star-fill"></i>
-                    <i className="bi bi-star-half"></i>
-                    <i className="bi bi-star-half"></i>
-                  </div>
-                  <div className="panel-body-meta">
-                    <h6>My Vouchers</h6>
-                    <div className="panel-body-content">
-                      <p>10% off any coffee</p>
-                      <p>25% off lunch offer 2pm</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <Button onClick={() => setOpenStarBucks(!openStarBucks)}>
+            Star Bucks
+            <div className="ratings">
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-half"></i>
+              <i className="bi bi-star-half"></i>
             </div>
-            <div className="panel panel-default">
-              <div
-                className="panel-heading"
-                data-toggle="collapse"
-                data-parent="#accordion"
-                // href="#collapse3"
-              >
-                <h4 className="panel-title">
-                  <a
-                    className="accordion-toggle"
-                    data-toggle="collapse"
-                    data-parent="#accordion"
-                    href="#collapse3"
-                  >
-                    Stellini's
-                    <div className="ratings">
-                      <span>2517 pts</span>
-                    </div>
-                  </a>
-                </h4>
-                <div className="panel-bottom">
-                  <h5>10% off Coffee</h5>
-                  <span>free</span>
+          </Button>
+          <Collapse in={openStarBucks}>
+            <Card>
+              <Card.Body>
+                <h3>Star Bucks</h3>
+                <div className="ratings">
+                  <i className="bi bi-star-fill"></i>
+                  <i className="bi bi-star-fill"></i>
+                  <i className="bi bi-star-fill"></i>
+                  <i className="bi bi-star-half"></i>
+                  <i className="bi bi-star-half"></i>
                 </div>
-                <i className="bi bi-chevron-down arrow-down"></i>
-              </div>
-              <div id="collapse3" className="panel-collapse collapse">
-                <div className="panel-body">
-                  <h3>Stellini's</h3>
-                  <div className="ratings">
-                    <strong>2517 pts</strong>
-                  </div>
-                  <div className="panel-body-meta">
-                    <h6>My Vouchers</h6>
-                    <div className="panel-body-content">
-                      <p>10% off any coffee</p>
-                      <p>25% off lunch offer 2pm</p>
-                    </div>
+                <div className="panel-body-meta">
+                  <h6>My Vouchers</h6>
+                  <div className="panel-body-content">
+                    <p>10% off any coffee</p>
+                    <p>25% off lunch offer 2pm</p>
                   </div>
                 </div>
-              </div>
+              </Card.Body>
+            </Card>
+          </Collapse>
+
+          <Button onClick={() => setOpenStellinis(!openStellinis)}>
+            Stellini's
+            <div className="ratings">
+              <strong>2517 pts</strong>
             </div>
-          </div>
+          </Button>
+          <Collapse in={openStellinis}>
+            <Card>
+              <Card.Body>
+                <h3>Stellini's</h3>
+                <div className="ratings">
+                  <strong>2517 pts</strong>
+                </div>
+                <div className="panel-body-meta">
+                  <h6>My Vouchers</h6>
+                  <div className="panel-body-content">
+                    <p>10% off any coffee</p>
+                    <p>25% off lunch offer 2pm</p>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Collapse>
         </div>
       </div>
       <Footer />
-
     </>
   );
 };
