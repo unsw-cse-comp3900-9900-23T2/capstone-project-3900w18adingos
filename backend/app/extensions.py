e966bbe130979a01e76dcd211519e028efa38021
Yskak <shiyap.yskak@gmail.com>
2023-07-28 15:47:50 +1000

    password reset fixes

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -16,4 +16,8 @@ def init_mail(app):
     app.config['MAIL_PASSWORD'] = 'Wakp9911'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
+    # RESET EMAIL CONFIG
+    app.config['PRAETORIAN_RESET_SENDER'] = 'jan.tulip1992@gmail.com'
+    app.config['PRAETORIAN_RESET_URI'] = 'http://localhost:5173/auth/reset-password' # Frontend Reset Password Page url
+    app.config['PRAETORIAN_RESET_SUBJECT'] = 'Password Reset Request'  
     mail.init_app(app)
